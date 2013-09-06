521be3124dadba2a56322629bcc6d9b20243021f
imoseyon <imoseyon@gmail.com>
2013-09-05 20:42:42 -0700

    ramdisk: better supersu (daemonsu) support

--- lk.ramdisk/sbin/runinitd.sh
+++ lk.ramdisk/sbin/runinitd.sh
@@ -1,8 +1,5 @@
 #!/sbin/bb/busybox ash
 
 # enable init.d and daemonsu support for stock ROM
-if [[ `grep -c corp.google.com /system/build.prop` -ge 1 ]]; then
-  /sbin/bb/busybox run-parts /system/etc/init.d
-  /system/xbin/daemonsu --auto-daemon &
-fi
-
+[[ `grep -c corp.google.com /system/build.prop` -ge 1 ]] && /sbin/bb/busybox run-parts /system/etc/init.d
+[ -f "/system/xbin/daemonsu" ] && /system/xbin/daemonsu --auto-daemon &
