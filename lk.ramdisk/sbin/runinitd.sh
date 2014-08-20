55ba4543e3057ce61a57d8ded273a31991a3a553
imoseyon <imoseyon@gmail.com>
2014-08-20 15:48:00 -0700

    ramdisk: replace random/urandom with erandom (from frandom)
    Conflicts:
    	lk.ramdisk/sbin/lk-post-boot.sh

--- lk.ramdisk/sbin/runinitd.sh
+++ lk.ramdisk/sbin/runinitd.sh
@@ -1,4 +1,11 @@
 #!/sbin/bb/busybox ash
+#
+# replace (u)random with erandom
+#
+rm -f /dev/urandom
+ln -s /dev/erandom /dev/urandom
+rm -f /dev/random
+ln -s /dev/erandom /dev/random
 
 # enable init.d and daemonsu support for stock ROM
 [[ `grep -c corp.google.com /system/build.prop` -ge 1 ]] && /sbin/bb/busybox run-parts /system/etc/init.d
